d25961c6b4606cb524ea2026cdd03d28d0e0d724
DAGroveGH <dagrove7@gmail.com>
2021-01-09 19:31:42 -0500

    Test

--- src/Testing/HelloDiscord.java
+++ src/Testing/HelloDiscord.java
@@ -3,5 +3,6 @@ package Testing;
 public class HelloDiscord {
     public static void main(String[] args) {
         System.out.println("Hello Discord!");
+        System.out.println("Test");
     }
 }
